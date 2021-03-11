7bff1dc07a3ed148b2979ac478055c37279c64af
C0ntRA1L <h31e26@gmail.com>
2021-03-11 14:09:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO.Ports;
+
+namespace RING
+{
+    class Program
+    {
+        static string SelectComPort()
+        {
+            bool flag = true;
+            int index,tmp;
+            string[] portList;
+            string selected;
+
+            try
+            {
+                Console.WriteLine("");
+                portList = SerialPort.GetPortNames();
+                for (index = 0; index < portList.Length; index++)
+                {
+                    Console.WriteLine("{0}:{1}",index,portList[index]);
+                }
+
+                do
+                {
+                    Console.Write("リスニングするCOMポートを選択>");
+                    selected = Console.ReadLine();
+                    if (int.TryParse(selected, out tmp) && tmp < portList.Length)
+                    {
+                        flag = false;
+                    }
+                    else
+                    {
+                        //範囲外，または存在しない
+                        continue;
+                    }
+                } while (flag);
+
+                return portList[tmp];
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SelectComPort();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
